34fc02a9978af0742f7856d804c13d436416e2fc
Michael Douglas <=>
2024-10-08 11:21:27 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,19 @@
 import Image from "next/image";
+import { format } from "date-fns"
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
-    <Header/>
+    <>
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Fulano!</h2>
+        <p className="capitalize">
+          {format(new Date(), "EEEE',' d 'de' MMMM 'de' yyyy", { locale: ptBR })}
+        </p>
+      </div>
+    </>
   );
 }
