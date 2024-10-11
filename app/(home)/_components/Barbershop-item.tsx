b6bf7885b1227b3e366a04534e646472e3c7d6b9
Michael Douglas <=>
2024-10-11 08:52:15 -0300

    feat: add initial raiting

--- app/(home)/_components/Barbershop-item.tsx
+++ app/(home)/_components/Barbershop-item.tsx
@@ -1,19 +1,27 @@
+import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
+import { StarIcon } from "lucide-react";
 import Image from "next/image";
 
 interface BarbershopItemProps {
     barbershop: Barbershop
 }
 
-const BarbershopItem = ({barbershop} : BarbershopItemProps) => {
-    
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+
     return (
         <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
             <CardContent className="px-1 py-0">
                 <div className="relative w-full h-[159px]">
-                    <Image src={barbershop.imageUrl} alt={barbershop.name} fill height={0} width={0} sizes="100vw" style={{objectFit: "cover"}} className="rounded-2xl" />
+                    <div className="absolute">
+                        <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-2 left-2 z-50">
+                            <StarIcon size={12} className="fill-primary text-primary" />
+                            <span>5,0</span>
+                        </Badge>
+                    </div>
+                    <Image src={barbershop.imageUrl} alt={barbershop.name} fill height={0} width={0} sizes="100vw" style={{ objectFit: "cover" }} className="rounded-2xl" />
                 </div>
 
                 <div className="px-2 pb-3">
@@ -25,5 +33,5 @@ const BarbershopItem = ({barbershop} : BarbershopItemProps) => {
         </Card>
     );
 }
- 
+
 export default BarbershopItem;
